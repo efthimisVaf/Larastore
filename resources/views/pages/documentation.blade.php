674cf001bb812ddcc3ecddde6d74cb39352ffef5
Efthimis <efthimis@pop-os.localdomain>
2020-11-29 05:05:07 +0100

    Added Pages

---
+++ resources/views/pages/documentation.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+    @section('content')
+    <h1>API</h1>
+    <p>This is the Documentation Page</p>
+    @endsection
---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+    @section('content')
+        <h1>Welcome!</h1>
+        <p><?php echo $welcome; ?></p>
+    @endsection
