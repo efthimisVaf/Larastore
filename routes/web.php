cb16491cd33181d1fac6e37ba1abbca1d8d28527
Efthimis <efthimis@pop-os.localdomain>
2020-12-10 06:44:05 +0100

    API Authentication

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,11 @@ Route::patch('/updateProdWithUi/{id}', [PagesController::class,'updateProduct'])
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
