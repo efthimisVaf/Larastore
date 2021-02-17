92a64a62cfdc8fb578c66d29e68953412332b878
Efthimis <efthimis@pop-os.localdomain>
2021-02-17 22:23:50 +0100

    documentation

--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,8 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{id}', [ProductController::class, 'show']);
-Route::delete('products/{id}', [ProductController::class, 'destroy'])->middleware('auth:api');
 Route::post('products', [ProductController::class, 'store'])->middleware('auth:api');
+Route::delete('products/{id}', [ProductController::class, 'destroy'])->middleware('auth:api');
 Route::put('products/{id}', [ProductController::class, 'update'])->middleware('auth:api');
 
 Route::get('vat', [VatTariffController::class, 'index']);
