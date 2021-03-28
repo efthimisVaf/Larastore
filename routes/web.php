17516701b6aed3e5a30034883d234e959dd42d44
Efthimis <efthimis@pop-os.localdomain>
2021-03-28 11:16:49 +0200

    register button fix

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PagesController::class,'welcome']);
 Route::get('/documentation', [PagesController::class,'documentation']);
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
 
 Route::get('/categories/list', [PagesController::class,'categoriesView']);
 Route::get('createCategoryView', [PagesController::class,'createCategory']);
