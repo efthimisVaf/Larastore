3710ee5acff03c3775d12b05c1a7859b0ef3d93e
Efthimis <efthimis@pop-os.localdomain>
2021-03-28 15:37:17 +0200

    login button fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,6 @@
                 <div class="card-body">
                     <form method="POST" action="/login">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -53,7 +52,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" href="/login">
+                                <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
